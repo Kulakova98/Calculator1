50800ed5ca879fe4a6e6b3e97cffa615721a9d72
Mariya Kulakova <mari.kulakova98@mail.ru>
2018-11-28 18:50:36 +0300

    ver 1.3. Skelet gotov

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,11 +5,27 @@
 #include "funcs.h"
 using namespace std;
 int main()
-{
-	/*ςσς ασδες ρκελες οπξγπΰμμϋ*/
-cout << MySum(a,b) << endl;
-cout << MySub(a,b) << endl;
-cout << Del(a,b) << endl;
-cout << Umn(a,b) << endl;
-return 0;
+{ 
+double a,b; 
+int n; 
+cout « "Menu: "; 
+cout « "Enter 2 numbers: "; 
+cin » a » b; 
+cout « "1. Summa"; 
+cout « "2. Vichitan"; 
+cout « "3. Delenie"; 
+cout « "4. Umnozhenie"; 
+cout « "Enter action:"; 
+cin » n; 
+switch (n) 
+{ 
+case '1': 
+cout « MySum(a,b) « endl; 
+case '2': 
+cout « MySub(a,b) « endl; 
+case '3': 
+cout « Del(a,b) « endl; 
+case '4': 
+cout « Umn(a,b) « endl; 
+return 0; 
 }
--- funcs.h
+++ funcs.h
@@ -5,5 +5,6 @@ double MySum(double, double);
 double MySub(double, double);
 double Del(double, double);
 double Umn(double, double);
-
+extern double a,b;
+extern int n;
 #endif // __FUNCS_H__
